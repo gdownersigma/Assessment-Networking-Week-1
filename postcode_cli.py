c538dab859ef402fe92ad5dceba68b302249cf3f
George <trainee.george.downer@sigmalabs.co.uk>
2025-11-07 15:26:51 +0000

    complete do_completion, pass all tests

--- postcode_cli.py
+++ postcode_cli.py
@@ -1,15 +1,24 @@
 """A CLI application for interacting with the Postcode API."""
 
 from argparse import ArgumentParser
-from postcode_functions import validate_postcode
+from postcode_functions import validate_postcode, get_postcode_completions
 
 
 def do_validation(postcode: str) -> str:
-    """Returns a string of whether the postcode is valid"""
+    """Returns a string of whether the postcode is valid."""
     formatted_postcode = postcode.strip().upper()
     if validate_postcode(formatted_postcode):
         return f"{formatted_postcode} is a valid postcode."
     return f"{formatted_postcode} is not a valid postcode."
+
+
+def do_completion(postcode: str) -> str:
+    """Returns a string of all the valid postcodes from the same base."""
+    formatted_postcode = postcode.strip().upper()
+    completions = get_postcode_completions(formatted_postcode)
+    if completions is None:
+        return f"No matches for {formatted_postcode}."
+    return '\n'.join([pc for pc in completions[:5]])
 
 
 parser = ArgumentParser()
@@ -21,3 +30,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.mode == 'validate':
         print(do_validation(args.postcode))
+    else:
+        print(do_completion(args.postcode))
