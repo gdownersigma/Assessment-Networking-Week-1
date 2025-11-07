6ce32e259ec881942eba398cb2cc4059e2221f5a
George <trainee.george.downer@sigmalabs.co.uk>
2025-11-07 15:11:06 +0000

    complete validation for CLI

--- postcode_cli.py
+++ postcode_cli.py
@@ -1,8 +1,23 @@
 """A CLI application for interacting with the Postcode API."""
 
 from argparse import ArgumentParser
+from postcode_functions import validate_postcode
+
+
+def do_validation(postcode: str) -> str:
+    """Returns a string of whether the postcode is valid"""
+    formatted_postcode = postcode.strip().upper()
+    if validate_postcode(formatted_postcode):
+        return f"{formatted_postcode} is a valid postcode."
+    return f"{formatted_postcode} is not a valid postcode."
+
 
 parser = ArgumentParser()
-parser.add_argument("-m", "--mode", choices=['validate', 'complete'])
+parser.add_argument("--mode", "-m", choices=['validate', 'complete'],
+                    help="Define whether you want validate mode or completion mode.", required=True)
+parser.add_argument("postcode", help="The postcode to validate or complete.")
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.mode == 'validate':
+        print(do_validation(args.postcode))
